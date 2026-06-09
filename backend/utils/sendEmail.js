4d901ef73da95d2587b97b1c438299762fe40387
Deepak413 <ddhaker2000@gmail.com>
2026-06-10 04:42:03 +0530

    Reset password sendEmail fix

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,33 +2,34 @@ const nodeMailer = require("nodemailer");
 
 const sendEmail = async (options) => {
     console.log("Options in sendEmail : ", options);
-    console.log("SMPT_MAIL in sendEmail : ", process.env.SMPT_MAIL);
-    console.log("SMPT_PASSWORD in sendEmail : ", process.env.SMPT_PASSWORD);
+    console.log("SMTP_MAIL in sendEmail : ", process.env.SMTP_MAIL);
+    console.log("SMTP_PASSWORD in sendEmail : ", process.env.SMTP_PASSWORD);
 
     let transporter = nodeMailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-            user: process.env.SMPT_MAIL,
-            pass: process.env.SMPT_PASSWORD,
+        service: 'Gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth:  {
+            user: process.env.SMTP_MAIL,
+            pass: process.env.SMTP_PASSWORD,
         }
     });
 
     const mailOptions = {
-        from: process.env.SMPT_MAIL,
+        from: process.env.SMTP_MAIL,
         to: options.email,
         subject: options.subject,
         text: options.message,
     };
-    await transporter.verify();
-    console.log("SMTP Connected in sendEmail");
+    // await transporter.verify();
+    // console.log("SMTP Connected in sendEmail");
 
     try {
         const info = await transporter.sendMail(mailOptions);
         console.log(info);
     } catch (err) {
         console.error("Mail Error:", err);
-        throw err;
     }
     // await transporter.sendMail(mailOptions);
 };
