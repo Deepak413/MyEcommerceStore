47b80df2fda5d6333a55bfacc4361c875dd90fa1
Deepak413 <ddhaker2000@gmail.com>
2026-07-05 02:19:27 +0530

    fix

--- backend/app.js
+++ backend/app.js
@@ -20,9 +20,10 @@ app.use(cors({
 dotenv.config({ path: "backend/config/config.env" });
 app.use(cookieParser());
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 //Route Imports
 const product = require("./routes/productRoute");
