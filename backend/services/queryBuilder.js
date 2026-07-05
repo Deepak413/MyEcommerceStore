129e0b66b22c1c78f8a7d7715af2915431e1c79d
Deepak413 <ddhaker2000@gmail.com>
2026-07-06 02:51:50 +0530

    fix

--- backend/services/queryBuilder.js
+++ backend/services/queryBuilder.js
@@ -5,17 +5,17 @@ exports.buildMongoQuery = (filters) => {
     query.category = filters.category;
   }
 
-  if (filters.name != null || filters.brand != null) {
-    const searchTerms = [];
+//   if (filters.name != null || filters.brand != null) {
+//     const searchTerms = [];
 
-    if (filters.name != null) searchTerms.push(filters.name);
-    if (filters.brand != null) searchTerms.push(filters.brand);
+//     if (filters.name != null) searchTerms.push(filters.name);
+//     if (filters.brand != null) searchTerms.push(filters.brand);
 
-    query.name = {
-      $regex: searchTerms.join("|"), // OR search
-      $options: "i",
-    };
-  }
+//     query.name = {
+//       $regex: searchTerms.join("|"), // OR search
+//       $options: "i",
+//     };
+//   }
 
   if (filters.priceMin != null || filters.priceMax != null) {
     query.price = {};
