cad772f639082a18acfd0e8c3b16c76a8fcdfbd3
Deepak413 <ddhaker2000@gmail.com>
2026-07-06 01:56:55 +0530

    fix

--- backend/services/queryBuilder.js
+++ backend/services/queryBuilder.js
@@ -1,61 +1,57 @@
 exports.buildMongoQuery = (filters) => {
-    const query = {};
+  const query = {};
 
-    if (filters.category) {
-        query.category = {
-            $regex: filters.category,
+  if (filters.category) {
+    query.category = filters.category;
+  }
 
-            $options: "i",
-        };
+  if (filters.name != null || filters.brand != null) {
+    const searchTerms = [];
+
+    if (filters.name != null) searchTerms.push(filters.name);
+    if (filters.brand != null) searchTerms.push(filters.brand);
+
+    query.name = {
+      $regex: searchTerms.join("|"), // OR search
+      $options: "i",
+    };
+  }
+
+  if (filters.priceMin != null || filters.priceMax != null) {
+    query.price = {};
+
+    if (filters.priceMin != null) {
+      query.price.$gte = filters.priceMin;
     }
 
-    if (filters.name || filters.brand) {
-        const searchTerms = [];
-
-        if (filters.name) searchTerms.push(filters.name);
-        if (filters.brand) searchTerms.push(filters.brand);
-
-        query.name = {
-            $regex: searchTerms.join("|"), // OR search
-            $options: "i",
-        };
+    if (filters.priceMax != null) {
+      query.price.$lte = filters.priceMax;
     }
+  }
 
-    if (filters.priceMin || filters.priceMax) {
-        query.price = {};
+  // Description search (color + usage + keywords)
+  // const descriptionTerms = [];
 
-        if (filters.priceMin) {
-            query.price.$gte = filters.priceMin;
-        }
+  // if (filters.color) descriptionTerms.push(filters.color);
 
-        if (filters.priceMax) {
-            query.price.$lte = filters.priceMax;
-        }
-    }
+  // if (filters.usage) descriptionTerms.push(filters.usage);
 
-    // Description search (color + usage + keywords)
-    const descriptionTerms = [];
+  // if (filters.keywords && filters.keywords.length > 0) {
+  //     descriptionTerms.push(...filters.keywords);
+  // }
 
-    if (filters.color) descriptionTerms.push(filters.color);
+  // if (descriptionTerms.length > 0) {
+  //     query.description = {
+  //         $regex: descriptionTerms.join("|"),
+  //         $options: "i",
+  //     };
+  // }
 
-    if (filters.usage) descriptionTerms.push(filters.usage);
+  if (filters.ratingMin != null) {
+    query.ratings = {
+      $gte: filters.ratingMin,
+    };
+  }
 
-    if (filters.keywords && filters.keywords.length > 0) {
-        descriptionTerms.push(...filters.keywords);
-    }
-
-    if (descriptionTerms.length > 0) {
-        query.description = {
-            $regex: descriptionTerms.join("|"),
-            $options: "i",
-        };
-    }
-
-    if (filters.ratingMin != null) {
-        query.ratings = {
-            $gte: filters.ratingMin,
-        };
-    }
-
-    return query;
+  return query;
 };
